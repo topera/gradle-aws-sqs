d37e2c5bc1d9b3a97ca04508075405868c8d85be
topera <topera@gmail.com>
2018-05-19 23:31:35 -0300

    Adding files

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ This example shows how to configure AWS SQS automatically and use it.
 * Go to AWS console and access IAM (Identity and Access Management).
 * Follow their instructions to create a "access key ID" and "secret access key" pair
 * In your machine, create this file to config your credentials: `~/.aws/credentials`
-* Add there a block like this one:
+* Add there a block like this one, using your keys:
 <pre>
 [default]
 aws_access_key_id = ADSEFDAXFFGFDSDFDSEE
 aws_secret_access_key = 5k3jskj4kjk2ldkfje/fdkjfrlkREs/ldsaREAsr
 </pre>
-* With the steps above we create a credential called 'default' that will be used to access AWS.
-* Now we'll run our Hello World that will create a new Queue in SQS and add 2 messages there
+* With the steps above we've created a credential called 'default' that will be used to access AWS.
+* Now we'll run our Hello World that will create a new Queue in SQS and add 3 messages there
 * Run `$ ./gradlew`
+* At this point, 3 messages were sent to the queue and 1 was received.
 * Now access AWS -> SQS e notice that we have a new queue created, with 2 messages.
+* Done!
 
 ## Tech Stack
 * Intellij IDEA 2018.1
@@ -23,6 +25,6 @@ aws_secret_access_key = 5k3jskj4kjk2ldkfje/fdkjfrlkREs/ldsaREAsr
 * Gradle 4.7
 
 ## Folder Structure
-TODO
+![folder-structure](./doc/files.png)
 
 To take a look in other projects, please see https://github.com/topera/index
--- src/main/java/com/topera/hello/Main.java
+++ src/main/java/com/topera/hello/Main.java
@@ -3,26 +3,57 @@ package com.topera.hello;
 import software.amazon.awssdk.core.auth.ProfileCredentialsProvider;
 import software.amazon.awssdk.core.regions.Region;
 import software.amazon.awssdk.services.sqs.SQSClient;
+import software.amazon.awssdk.services.sqs.model.CreateQueueRequest;
+import software.amazon.awssdk.services.sqs.model.CreateQueueResponse;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 public class Main {
 
     private static final Region REGION = Region.US_EAST_1;
     private static final String PROFILE_NAME = "default";
+    private static final String HELLO_WORLD_QUEUE = "hello-world-queue";
 
     public static void main(String[] args) {
         // create sqs client
         SQSClient sqsClient = createSQSClient();
 
+        // create queue and get its url
+        CreateQueueResponse queue = sqsClient.createQueue(createCreateQueue());
+        String queueUrl = queue.queueUrl();
+        System.out.println("Queue created with url " + queueUrl);
+
         // create and send messages
-        sqsClient.sendMessage(createMessage("message-1"));
-        sqsClient.sendMessage(createMessage("message-2"));
+        sqsClient.sendMessage(createMessage(queueUrl, "message-1"));
+        sqsClient.sendMessage(createMessage(queueUrl, "message-2"));
+        sqsClient.sendMessage(createMessage(queueUrl, "message-3"));
+        System.out.println("Three messages sent");
+
+        // get one message from queue
+        ReceiveMessageResponse receiveMessageResponse = sqsClient.receiveMessage(createReceiveMessageRequest(queueUrl));
+        System.out.println("Body of first message: " + receiveMessageResponse.messages().get(0).body());
+
+        System.out.println("There are still at least 2 messages in the queue. Please check AWS console.");
+
     }
 
-    private static SendMessageRequest createMessage(String messageBody) {
+    private static ReceiveMessageRequest createReceiveMessageRequest(String queueUrl) {
+        return ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .build();
+    }
+
+    private static CreateQueueRequest createCreateQueue() {
+        return CreateQueueRequest.builder()
+                .queueName(HELLO_WORLD_QUEUE)
+                .build();
+    }
+
+    private static SendMessageRequest createMessage(String queueUrl, String messageBody) {
         return SendMessageRequest.builder()
                 .messageBody(messageBody)
-                .queueUrl("https://sqs.us-east-1.amazonaws.com/727193322906/test-queue")
+                .queueUrl(queueUrl)
                 .build();
     }
 
